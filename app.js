55a03e25c0ad3991e80807629ed259866f639813
snegmen <che1376@mail.ru>
2019-09-18 11:15:52 +0700

    small fix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use('/cards', cardsRouter);
 app.use('/users', usersRouter);
 app.use('*', (req, res) => {
-  res.status(404).send({ messege: 'Запрашиваемый ресурс не найден' });
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 app.listen(PORT);
